2d4830cc3d924bd9b11d051d1ef1f70e7a5514a2
Eliab E <eliaberango@gmail.com>
2024-02-28 10:47:05 -0800

    Implement a function to post signup base on user inserted data

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -148,3 +148,28 @@ export const checkAuthenticated = () => async dispatch => {
         });
     }
 };
+
+export const signup = (first_name, last_name, phone, email, password, re_password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const body = JSON.stringify({ first_name, last_name, phone, email, password, re_password });
+
+    try {
+        const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/users/`, body, config);
+
+        dispatch({
+            type: SIGNUP_SUCCESS,
+            payload: res.data
+        });
+    } catch (err) {
+        console.log(err);
+        dispatch({
+            type: SIGNUP_FAIL
+        })
+    }
+};
+
