eaca47a9300c160df61cd3d1b7f1d5f7266187e6
Eliab E <eliaberango@gmail.com>
2024-02-28 10:41:17 -0800

    Implement a function to request login

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -89,4 +89,29 @@ export const load_user = () => async dispatch => {
             type: USER_LOADED_FAIL
         });
     }
-}
+};
+
+export const login = (email, password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const body = JSON.stringify({ email, password });
+
+    try {
+        const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/jwt/create/`, body, config);
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+        dispatch(load_user());
+    } catch (err) {
+        dispatch({
+            type: LOGIN_FAIL
+        });
+    }
+};
+
