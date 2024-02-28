c0956ebe38277c3cd802dc75847d24b70e73e307
Eliab E <eliaberango@gmail.com>
2024-02-28 06:04:23 -0800

    Add classes used in the admin site to manage data on the database created from models

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -2,4 +2,34 @@
     different data on the database
 """
 from django.contrib import admin
+from accounts.models import UserAccount, Complaint, CompTypes, Region
 
+
+class UserAdmin(admin.ModelAdmin):
+    """Class defines display list items in user model
+    """
+    list_display = ['email', 'first_name', 'last_name', 'phone']
+
+
+class ComplaintAdmin(admin.ModelAdmin):
+    """Class defines display list items of complaint model
+    """
+    list_display = ['id', 'compType']
+
+
+class CompTypesAdmin(admin.ModelAdmin):
+    """Class defines display list items of complaint types model
+    """
+    list_display = ['id', 'name']
+
+
+class RegionAdmin(admin.ModelAdmin):
+    """Class defines display list items of region model
+    """
+    list_display = ['id', 'name']
+
+
+admin.site.register(UserAccount, UserAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
+admin.site.register(CompTypes, CompTypesAdmin)
+admin.site.register(Region, RegionAdmin)
