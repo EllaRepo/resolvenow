7369d40815db7714cd18633c312de97ade99ea8b
Eliab E <eliaberango@gmail.com>
2024-02-28 04:11:05 -0800

    add url patterns for endpoints

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,5 +5,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('postComp/', views.ComplaintView.as_view(), name='comp_register'),
+    path('region/', views.getRegions, name='region'),
+    path('ctypes/', views.getCompTypes, name='ctypes'),
+    path('complaints/<email>', views.complaints, name='complaints'),
+    path('update/<email>', views.updateUserProfile, name='userProfile'),
+    path('updateComp/<id>', views.updateComp, name='complaintStatus'),
 ]
-                                                                   
