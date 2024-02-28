77c8d9e0516d1ce8b83cbfdf2ff4c10570ed90ab
Eliab E <eliaberango@gmail.com>
2024-02-28 01:28:57 -0800

    Add a view for complaint registration and complaint update

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,2 +1,55 @@
 """This module defines the views for account app
 """
+from accounts.models import Complaint, CompTypes, Region, UserAccount
+from accounts.serializers import ComplaintSerializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.parsers import FormParser, MultiPartParser
+
+
+class ComplaintView(generics.CreateAPIView):
+    """Complaint registering view
+    """
+    queryset = Complaint.objects.all()
+    serializer_class = ComplaintSerializer
+
+
+@api_view(['GET'])
+@renderer_classes((JSONRenderer,))
+@permission_classes([IsAuthenticated])
+def complaints(request, email=None):
+    """Returns complaints
+    """
+    if request.method == 'GET':
+        cmplts = []
+        for obj in Complaint.objects.all():
+            if obj.__dict__['email'] == email:
+                cmplts.append(obj.to_dict())
+        return Response({'data': cmplts}, status=status.HTTP_200_OK)
+    return Response({}, status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+@renderer_classes((JSONRenderer,))
+@permission_classes([IsAuthenticated])
+def updateComp(request, id=None):
+    """Returns status
+    """
+    if request.method == 'PUT':
+        comps = Complaint.objects.all()
+        print(request.data)
+        if request.data and request.data != {}:
+            for obj in comps:
+                if str(obj.__dict__['id']) == str(id):
+                    print(obj.__dict__['id'])
+                    print(id)
+                    for key, value in request.data.items():
+                        setattr(obj, key, value)
+                    obj.save()
+                    return Response({}, status=status.HTTP_200_OK)
+    return Response({}, status.HTTP_400_BAD_REQUEST)
