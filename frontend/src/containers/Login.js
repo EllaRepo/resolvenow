36056ecc8aac92d7cc8f6a56c3ec2994b312c77e
Eliab E <eliaberango@gmail.com>
2024-02-28 22:39:28 -0800

    Adjust items alignment, positioning of login container

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -24,42 +24,90 @@ const Login = ({ login, isAuthenticated, user }) => {
     }
 
     return (
-        <div className='container mt-4'>
-            <h1>Sign In</h1>
-            <p> Sign into your Account</p>
-            <form onSubmit={e => onSubmit(e)}>
-                <div className='form-group'>
-                    <input
-                        className='form-control'
-                        type='email'
-                        placeholder='Email'
-                        name='email'
-                        value={email}
-                        onChange={e => onChange(e)}
-                        required
-                    />
-                </div>
-                <div className='form-group'>
-                    <input
-                        className='form-control'
-                        type='password'
-                        placeholder='Password'
-                        name='password'
-                        value={password}
-                        onChange={e => onChange(e)}
-                        minLength='8'
-                        required
-                    />
-                </div>
-                <button className='btn btn-primary' type='submit'>Login</button>
-            </form>
-            <p className='mt-4'>
-                Don't have an account? <Link to='/signup'>Sign Up</Link>
-            </p>
-            <p className='mt-4'>
-                Forget your Password? <Link to='/reset-password'>Reset Password</Link>
-            </p>
+        <div>
+            <>
+                <section className="">
+                    <div className="py-4">
+                        <div className=" d-flex justify-content-center align-items-center">
+                            <div className="">
+                                <div className="card bg_color2" style={{ borderRadius: "1rem", backgroundColor: "#78A083" }}>
+                                    <div className="d-flex align-items-center">
+                                        <div className="card-body p-2 p-lg-4 text-black">
+                                            <form onSubmit={onSubmit}>
+                                                <div className="d-flex justify-content-center mb-3 pb-1">
+                                                    <div className="d-flex justify-content-center mb-1 pb-1">
+                                                        <span className="h2 fw-bold mb-0">Login 👋</span>
+                                                    </div>
+                                                </div>
+                                                <h5
+                                                    className="d-flex justify-content-center fw-normal mb-3 pb-3"
+                                                    style={{ letterSpacing: 1 }}
+                                                >
+                                                    Sign into your Account
+                                                </h5>
+                                                <div className="form-outline mb-4">
+                                                    <input
+                                                        className='form-control form-control-lg'
+                                                        type='email'
+                                                        placeholder='Email'
+                                                        name='email'
+                                                        value={email}
+                                                        onChange={e => onChange(e)}
+                                                        required
+                                                    />
+                                                    <label className="form-label" htmlFor="form2Example17">
+                                                        Email address
+                                                    </label>
+                                                </div>
+                                                <div className="form-outline mb-2">
+                                                    <input
+                                                        className='form-control form-control-lg'
+                                                        type='password'
+                                                        placeholder='Password'
+                                                        name='password'
+                                                        value={password}
+                                                        onChange={e => onChange(e)}
+                                                        required
+                                                    />
+                                                    <label className="form-label" htmlFor="form2Example27">
+                                                        Password
+                                                    </label>
+                                                </div>
+                                                <div className="d-flex justify-content-center mb-2">
+                                                    <button
+                                                        className="btn btn-primary btn-lg btn-block"
+                                                        type="submit"
+                                                    >
+                                                        Login
+                                                    </button>
+                                                </div>
+                                                <p className="" style={{}}>
+                                                    Don't have an account?{" "}
+                                                    <Link to="/signup" style={{}}>
+                                                        Sign Up
+                                                    </Link>
+                                                </p>
+                                                <p className=''>
+                                                    Forget your Password? <Link to='/reset-password'>Reset Password</Link>
+                                                </p>
+                                                <div className='d-flex justify-content-center'>
+                                                    <Link to="#" className="small text-muted">
+                                                        Terms of use.
+                                                    </Link>
+                                                    <Link to="#" className="small text-muted">
+                                                        Privacy policy
+                                                    </Link>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </>
         </div>
     )
 };
